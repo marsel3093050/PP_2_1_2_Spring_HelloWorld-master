2976bee18d64dfddf67ff2c9415f898d870110cc
Marsel Fayzullin <marsel522@yandex.ru>
2023-02-20 22:20:37 +0500

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+
+        System.out.println("HelloWorld equality check: " + (bean1 == bean2));
+        System.out.println("Cat equality check: " + (cat1 == cat2));
+
+
+    }
+}
